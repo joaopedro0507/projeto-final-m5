4a034d7e595980d87d254a4320415d73ccba587c
Luana13-bit <your.emailluanarafaela1001@gmail.com>
2025-10-07 19:30:20 -0300

    commit teste

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
+          <p>Luana passou por aqui!</p>
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
