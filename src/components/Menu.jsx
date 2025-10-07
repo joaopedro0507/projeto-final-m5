7fd662b0470532a124844aa575da0d06fcbec1f5
Luana13-bit <your.emailluanarafaela1001@gmail.com>
2025-10-07 20:40:45 -0300

    add menu teste

---
+++ src/components/Menu.jsx
@@ -0,0 +1,11 @@
+export default function Menu() {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Sobre</li>
+        <li>Mapeamento do Racismo </li>
+      </ul>
+    </nav>
+  );
+}
