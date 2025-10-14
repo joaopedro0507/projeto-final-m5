47d24e35f5c191a9180c99a5e383dccc09588b61
Luana13-bit <your.emailluanarafaela1001@gmail.com>
2025-10-14 15:06:34 -0300

    arrumando app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import Menu from './components/Menu.jsx';
-import Campanha from './components/Denuncia.jsx';
-import Denuncia from './components/Campanha.jsx';
-import './App.css';
+import Menu from "./components/Menu/Menu.jsx";
+import Campanha from "./components/Campanha/Campanha.jsx";
+import Denuncia from "./components/Denuncia/Denuncia.jsx";
+
 
 function App() {
   return (
